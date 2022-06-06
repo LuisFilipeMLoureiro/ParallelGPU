5eeb0f6c030b333a66d08bf241044ecc30caef49
Luís Filipe Martins Loureiro <luisfml@al.insper.edu.br>
2022-06-06 12:18:35 -0300

    Final version of GPU Brute Force

--- main.cu
+++ main.cu
@@ -8,21 +8,11 @@
 
 using namespace std;
 
-
-struct Indexes{
-
-    int i;
-    int j;
-    int size;
-};
-
-
-
 struct custom_transform
 {
     __host__ __device__
 
-    double operator()(const char &a, const char &b)
+    double operator()(const char& a, const char& b)
     { 
         if (a == b)
         {
@@ -35,48 +25,6 @@ struct custom_transform
 };
 
 
-
-
-// https://stackoverflow.com/questions/15726641/find-all-possible-substring-in-fastest-way
-vector<string> subs_generator(string DNA, int size){
-    vector<string> lista_subs;
-    
-    for(int i = 0; i < size; i++){
-        for(int j = i + 1; j < size; j++){
-
-            string x = DNA.substr(i,j);           
-            lista_subs.push_back(x);
-            
-        }
-        
-        lista_subs.push_back(DNA);
-        
-    }
-    return lista_subs;
-}
-
-vector<Indexes> index_generator(string DNA, int size)
-{
-
-    vector<Indexes> IndexList;
-    Indexes index;
-
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = i + 1; j < size; j++)
-        {
-            index.i = i;
-            index.j = j;
-            index.size = j - i;
-
-            IndexList.push_back(index);
-        }
-    }
-
-
-    return IndexList;
-
-}
 
 
 
@@ -106,20 +54,13 @@ int main()
         max = n;
     }
 
-    all_SeqA = subs_generator(SeqA, n);
-    all_SeqB = subs_generator(SeqB, m);
+    
 
 
-    int cand_SeqA, cand_SeqB, id, contador, candidato, Asize, Bsize;
-    string finalSeqA, finalSeqB; 
-    contador = 0;
-    cand_SeqA = 0;
-    cand_SeqB = 0;
+    int  Asize, Bsize;
+     
+    
     int match = 0;
-
-
-
-
    
 
 
@@ -132,46 +73,47 @@ int main()
     std::sort(all_SeqB.begin(), all_SeqB.end());
     all_SeqB.erase(std::unique(all_SeqB.begin(), all_SeqB.end()), all_SeqB.end());
 
-
-    vector<Indexes> IndexSeqA = index_generator(SeqA, n);
-    vector<Indexes> IndexSeqB = index_generator(SeqB, m);
-
-
-    vector<char> VSeqA;
-    vector<char> VSeqB;
-
-    for(auto&A:SeqA){
-    	VSeqA.push_back(A);		
-    }
-    for(auto&B:SeqB){
-    	VSeqB.push_back(B);
-    }
+    thrust::device_vector<char> SeqA_GPU(n);
+    thrust::device_vector<char> SeqB_GPU(m);
+    thrust::device_vector<int> MatchVec(max);
 
 
-    thrust::device_vector<char> SeqA_GPU(VSeqA);
-    thrust::device_vector<char> SeqB_GPU(VSeqB);
-    thrust::device_vector<int> MatchVec(m);
+for(int r=0; r<n; r++){
+	SeqA_GPU[r] = SeqA[r];		
+}
+	for(int y=0; y<m;y++){
+	SeqB_GPU[y] = SeqB[y];
+}
+
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+
+            Asize = j - i;
+
+            for (int i_B = 0; i_B < m; i_B++) {
+
+                for (int j_B = i_B + 1; j_B < m; j_B++){
 
 
+                    Bsize = j_B - i_B;
 
-    for (auto&Seq_A:IndexSeqA)
-    {
-        for(auto&Seq_B:IndexSeqB)
-        {
-            if (Seq_A.size == Seq_B.size)
-            {
-                thrust::transform(SeqA_GPU.begin() + Seq_A.i, SeqA_GPU.begin() + Seq_A.j, SeqB_GPU.begin() + Seq_B.i, MatchVec.begin(), custom_transform());
+                    if (Asize == Bsize) {	
+			
+                        thrust::transform(SeqA_GPU.begin() + i, SeqA_GPU.begin() + j, SeqB_GPU.begin() + i_B, MatchVec.begin(), custom_transform());
 
-                int score = thrust::reduce(MatchVec.begin(), MatchVec.end(),0, thrust::plus<int>());
-                
-                if (score > match)
-                {
-                    match = score;
+                        int score = thrust::reduce(MatchVec.begin(), MatchVec.begin() +Bsize ,0, thrust::plus<int>());
+                        
+                        if (score > match) {
+                            match = score;
+                        }
+                    }
                 }
-            }
+            }   
         }
     }
+    
 
+    
 
     cout << "Resultados Finais:"<< endl;
     cout << "Match Max:"<< endl;
@@ -183,4 +125,3 @@ int main()
 
     return 0;
 }
-
